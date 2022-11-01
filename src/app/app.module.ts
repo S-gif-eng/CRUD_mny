e4c6126285c387fdf54ded440592acdf3110826a
Inthiyaz <skinthiyaz9581@gmail.com>
2022-11-01 17:20:09 +0530

    inthiyaz_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './core/login/login.component';
+import { SignupComponent } from './core/signup/signup.component';
+import { ForgetComponent } from './core/forget/forget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgetComponent
   ],
   imports: [
     BrowserModule,
